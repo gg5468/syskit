1e1c96186ebf745e458f03a000537a707a78fa82
gg307 <domagojbirkic353@outlook.com>
2024-06-03 01:55:34 +0200

    makes web app deploy python 3.11 on linux

--- createwebapp.ps1
+++ createwebapp.ps1
@@ -6,4 +6,6 @@ param(
 Install-Module -Name Az -Repository PSGallery #-Force
 Connect-AzAccount
 New-AzWebApp -ResourceGroupName $resourceGroupName -Name $webAppName -AppServicePlan $webAppPlanName
- 
+$config = Get-AzResource -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Web/sites/config -ResourceName $webAppName -ApiVersion 2023-12-01
+$config.Properties.linuxFxVersion = "python|3.11"
+$config | Set-AzResource -ApiVersion 2023-12-01 -Force
